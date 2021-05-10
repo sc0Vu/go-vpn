ef08d60826fc87333c9df60dad62b00d9abb02ba
sc0Vu <alk03073135@gmail.com>
2021-05-10 19:13:02 +0800

    Add openvpn

--- main.go
+++ main.go
@@ -2,12 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 
+	"github.com/mysteriumnetwork/go-openvpn/openvpn3"
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
 )
+
+type callbacks interface {
+	openvpn3.Logger
+	openvpn3.EventConsumer
+	openvpn3.StatsConsumer
+}
+
+type loggingCallbacks struct {
+}
+
+func (lc *loggingCallbacks) Log(text string) {
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		fmt.Println("Openvpn log >>", line)
+	}
+}
+
+func (lc *loggingCallbacks) OnEvent(event openvpn3.Event) {
+	fmt.Printf("Openvpn event >> %+v\n", event)
+}
+
+func (lc *loggingCallbacks) OnStats(stats openvpn3.Statistics) {
+	fmt.Printf("Openvpn stats >> %+v\n", stats)
+}
+
+var (
+	_        callbacks = &loggingCallbacks{}
+	fileName string    = ""
+)
+
+type StdoutLogger func(text string)
+
+func (lc StdoutLogger) Log(text string) {
+	lc(text)
+}
 
 func createSettingWindow() *widgets.QMainWindow {
 	window := widgets.NewQMainWindow(nil, 0)
@@ -25,22 +63,47 @@ func createSettingWidget() *widgets.QWidget {
 	input.SetEnabled(false)
 	widget.Layout().AddWidget(input)
 
+	startButton := widgets.NewQPushButton2("Start", nil)
+	startButton.ConnectClicked(func(bool) {
+		startButton.SetEnabled(false)
+		var logger StdoutLogger = func(text string) {
+			lines := strings.Split(text, "\n")
+			for _, line := range lines {
+				fmt.Println("Library check >>", line)
+			}
+		}
+
+		openvpn3.SelfCheck(logger)
+
+		bytes, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		config := openvpn3.NewConfig(string(bytes))
+
+		session := openvpn3.NewSession(config, openvpn3.UserCredentials{}, &loggingCallbacks{})
+		session.Start()
+		err = session.Wait()
+		if err != nil {
+			fmt.Println("Openvpn3 error: ", err)
+		} else {
+			fmt.Println("Graceful exit")
+		}
+		startButton.SetEnabled(true)
+	})
+	startButton.SetEnabled(false)
+	widget.Layout().AddWidget(startButton)
+
 	fileButton := widgets.NewQPushButton2("Select .ovpn file", nil)
 	fileButton.ConnectClicked(func(bool) {
 		fileDialog := widgets.NewQFileDialog(nil, core.Qt__Window)
-		fileName := fileDialog.GetOpenFileName(nil, "Select .ovpn file", "", "*.ovpn", "", widgets.QFileDialog__ReadOnly)
+		fileName = fileDialog.GetOpenFileName(nil, "Select .ovpn file", "", "*.ovpn", "", widgets.QFileDialog__ReadOnly)
 		input.SetPlaceholderText(fileName)
 		fileButton.SetEnabled(false)
 		startButton.SetEnabled(true)
 	})
 	widget.Layout().AddWidget(fileButton)
-
-	startButton := widgets.NewQPushButton2("Start", nil)
-	startButton.ConnectClicked(func(bool) {
-		// start vpn
-	})
-	startButton.SetEnabled(false)
-	widget.Layout().AddWidget(startButton)
 	return widget
 }
 
